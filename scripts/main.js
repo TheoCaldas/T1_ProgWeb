c5d5fc54ec4b64fb8f03b606978d3a15e7fcb628
TheoCaldas <theocaldas28@gmail.com>
2022-09-21 00:08:30 -0300

    [ADD] Email validation

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,18 @@
-function playButtonSelected() {
-    alert("oi")
+onload = function(){
+    document.getElementById("playButton").addEventListener("click", confirmEmail);
+}
+
+function confirmEmail(){
+    var emailElement = document.getElementById("emailField");
+    var typedEmail = emailElement.value;
+    var re = /^\[^aeiou]+\[[^aeiou]+|([^aeiou]+|).[^aeiou]+\]$/;
+    // var re = /^\[qwrtypsdfghjklzxcvbnm]+\[[qwrtypsdfghjklzxcvbnm]+|([qwrtypsdfghjklzxcvbnm]+|).[qwrtypsdfghjklzxcvbnm]+\]$/;
+    // console.log(re.test(email.toLowerCase())); 
+    if (!re.test(typedEmail.toLowerCase()))
+        emailElement.style.outlineColor = "red";
+    else
+    {
+        emailElement.style.outlineColor = "black";
+        console.log("go to next page");
+    }
 }
